8ddbf276522acb94d1d63b3aa39483a745f80cc2
Danchuk-Lauzon, Alexy <adanchuklauzon@lcs-mba3080.lcs.on.ca>
2018-04-09 21:25:53 -0400

    All posible inputs filtered (Finished) v.1

--- RotatingLetters/main.swift
+++ RotatingLetters/main.swift
@@ -11,7 +11,70 @@ import Foundation
 // Get the user input
 var rawInput = readLine()
 
-// Print out the input provided
-print("You said:")
-print(rawInput)
+// Makes sure that the input is a String and not an Int or nil
+guard let input : String = rawInput else{
+   print("NO")
+    exit(9)
+}
+
+
+// Checks to see if the input is in upper case
+var upperInput = input.uppercased()
+if input != upperInput {
+    print("NO")
+    exit(9)
+}
+
+
+// Checks to see if the length of the input is acceptable
+if input.characters.count > 30 || input.characters.count < 1 {
+    print("NO")
+    exit(9)
+}
+
+// creating a variable to compare the number of acceptable letters to the length of the input to determine is they match.
+var goodCharacterNumber = 0
+
+for individualCharacters in input {
+    
+    
+    switch individualCharacters {
+    case "I", "O", "S", "H", "Z", "X", "N" :
+        goodCharacterNumber += 1
+    case " " :
+        break
+    
+    default: break
+    }
+    
+}
+
+// If they match than they are good if not program ends
+if input.characters.count == goodCharacterNumber {
+    print("YES")
+} else {
+    print("NO")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
